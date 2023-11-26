128bcdeb1165eb733b014fd6490c9c1e1f9e74ff
yosiefgobeze <61394128+yosiefgobeze@users.noreply.github.com>
2023-11-26 19:05:14 +0000

    stream api filter demo one

--- FilterDemoOne.java
+++ FilterDemoOne.java
@@ -1,5 +1,6 @@
 import java.util.*;
-import java.util.stream;
+import java.util.stream.Collectors;
+
 public class FilterDemoOne{
   public static void main(String[] me){
     // steps to use stream api
@@ -10,9 +11,13 @@ public class FilterDemoOne{
     List<Integer> ages = Arrays.asList(23, 22, 40, 38, 18, 15, 28, 31);
     List<Integer> evenAges = new ArrayList<>();
     evenAges = ages.stream().filter(n -> (n%2 == 0)).collect(Collectors.toList());
+    System.out.println("Filtered and collected.....");
     for(int age: evenAges){
     System.out.println(age);
     }
+    System.out.println("Filtered and displayed.....");
+    // if yo need to print the filtered values
+    ages.stream().filter(a -> (a%2==0)).forEach(a -> System.out.println(a));
 
   }
 }
